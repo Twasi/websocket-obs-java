0d793158e041b150d6a3a9fce8ff334a2eccabfd
TinaTiel <contact@tinatiel.com>
2021-07-09 10:51:08 +0200

    test: outlined rest of general category e2e tests

--- client/src/endToEndSecuredTest/java/io/obswebsocket/community/client/test/message/GeneralE2eIT.java
+++ client/src/endToEndSecuredTest/java/io/obswebsocket/community/client/test/message/GeneralE2eIT.java
@@ -1,6 +1,7 @@
 package io.obswebsocket.community.client.test.message;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Fail.fail;
 
 import io.obswebsocket.community.client.OBSRemoteController;
 import io.obswebsocket.community.client.message.response.general.GetVersionResponse;
@@ -10,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class GeneralE2eIT extends AbstractObsE2ETest {
@@ -47,5 +49,63 @@ public class GeneralE2eIT extends AbstractObsE2ETest {
 
   }
 
+  @Test
+  void broadcastCustomEvent() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void getSystemStats() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void getHotkeyList() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void triggerHotkeyByName() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void triggerHotkeyBySequence() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void getProjectorList() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void openCloseProjector() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void getSetStudioModeEnabled() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void sleep() {
+    fail("not implemented");
+  }
+
+  @Disabled
+  @Test
+  void exitStarted() {
+    fail("not implemented");
+  }
 
 }
